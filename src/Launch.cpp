9b0b17f9a7808a964f47c322fce47d190378c561
Matt Smith <S3769858@student.rmit.edu.au>
2022-12-03 17:19:43 +1100

    Update Launch.cpp
    Limited framerate to observe color changes, also updated draws to come from an Index Buffer rather than Vertex Buffer and set color based on Uniform

--- src/Launch.cpp
+++ src/Launch.cpp
@@ -83,6 +83,9 @@ static bool initGLFW()
     // Set what function to call when the window is resized, passed as a parameter
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Set framerate
+    glfwSwapInterval(10);
+
     return true;
 }
 
@@ -203,10 +206,17 @@ int main()
         0.5f, -0.5f
     };
 
+    unsigned int indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
+    };
+
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
+    // Vertex Buffer
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
@@ -214,11 +224,21 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    // Index Buffer
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
     // Create Shaders and link to a program created on the GPU
     createShaders();
 
     glUseProgram(shaderProgram);
 
+    int location = glGetUniformLocation(shaderProgram, "u_Color");
+    glUniform4f(location, 0.0f, 1.0f, 0.0f, 1.0f);
+
+    float r = 0.0f;
+    float increment = 0.05f;
     // Render Loop, will loop through until the window is closed
     while (!glfwWindowShouldClose(window))
     {
@@ -227,8 +247,22 @@ int main()
 
         // Draw code
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        glUniform4f(location, r, 0.0f, 1.0f, 1.0f);
+
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        if (r > 1.0f)
+        {
+            increment = -0.05f;
+        }
+        else if (r < 0.0f)
+        {
+            increment = 0.05f;
+        }
+
+        r += increment;
 
         // Swap the buffers
         glfwSwapBuffers(window);
